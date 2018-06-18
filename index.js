28d2f85a1512834942397aa5fa8b4233cb241ae9
ertyu5 <40370093+ertyu5@users.noreply.github.com>
2018-06-18 22:29:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,22 @@
+
 const discord = require('discord.js'),
 client = new discord.Client();
 const token = (process.env.TOKEN);
-client.on("ready", () => {
-});
+const prefix = '>';
 
-client.on("ready", () => {
-client.user.setPresence({ game: { name: ' #Pirate ou #Princesse ! ', type: 0}});
+client.on('ready', () => {
+  console.log('Je suis réveiller!');
+  client.user.setActivity("Natthh est grand");
 });
-
-client.login(token)
 
 client.on('message', message => {
-
-var TeamPirates = client.guilds.get(message.guild.id).roles.find("name", "Team Pirates");
-var TeamPrincesses = client.guilds.get(message.guild.id).roles.find("name", "Team Princesses");
-
-const PirateEmoji = client.emojis.find("name", "Emoji_Props_Universal1L");
-const PrincesseEmoji = client.emojis.find("name", "EmojiBlitzJasmine");
-
-if(message.content.toLocaleLowerCase() == ("#pirate")){
- message.channel.send(`Tu es maintenant dans la team Pirates ${message.author} ${PirateEmoji} !`);
- message.guild.member(message.author).addRole(TeamPirates);
- message.guild.member(message.author).removeRole(TeamPrincesses);
- }
-
- if(message.content.toLocaleLowerCase() == ("#princesse")){
- message.channel.send(`Tu es maintenant dans la team Princesses ${message.author} ${PrincesseEmoji} !`);
- message.guild.member(message.author).addRole(TeamPrincesses);
- message.guild.member(message.author).removeRole(TeamPirates);
- }
-
+  if (!message.guild) return;
 });
+
+
+client.on('guildMemberAdd', member =>{
+    member.guild.channels.get('442332780106612778').send('**' + member.user.username + ' , a rejoind le serveur !**');
+    let role = message.guild.roles.get("330100338042470404");
+    member.addRole(role)
+});
+client.login(token);
