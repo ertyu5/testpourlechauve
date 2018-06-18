812cb398218bff8dc4add0d9c636c0fb540ad822
ertyu5 <40370093+ertyu5@users.noreply.github.com>
2018-06-18 22:36:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const discord = require('discord.js'),
 client = new discord.Client();
 const token = (process.env.TOKEN);
@@ -5,7 +6,7 @@ const prefix = '>';
 
 client.on('ready', () => {
   console.log('Je suis réveiller!');
-  client.user.setActivity("Natthh est grand");
+  client.user.setActivity("Admirer ce beau serveur");
 });
 
 client.on('message', message => {
@@ -13,19 +14,9 @@ client.on('message', message => {
 });
 
 
-client.on("message", message => {
-
-if (message.content.includes("Natthh le grand roi")) {
-if(message.author.bot) return;
-client.channels.get('345327498592256000').send(`Oui, Natthh est un très grand roi !`)
-}
-
-});
-
 client.on('guildMemberAdd', member =>{
     member.guild.channels.get('442332780106612778').send('**' + member.user.username + ' , a rejoind le serveur !**');
     let role = message.guild.roles.get("330100338042470404");
     member.addRole(role)
 });
-
 client.login(token);
